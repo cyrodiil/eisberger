81eb3a845955339eb1f7116be5aaab6eb9ddd4af
Raphael Dürst <raphael.duerst@gmail.com>
2014-11-11 10:51:30 +0100

    Added css classes to panel layout, image and text smaller

--- sites/all/themes/eisberger/panels/layouts/eisberger/eisberger_fourcol_3_9/eisberger-fourcol-3-9.tpl.php
+++ sites/all/themes/eisberger/panels/layouts/eisberger/eisberger_fourcol_3_9/eisberger-fourcol-3-9.tpl.php
@@ -1,63 +1,63 @@
 <div class="panel-display omega-grid eisberger-fourcol-3-9" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel grid-12">
+  <div class="panel-panel grid-12 eisberger-top">
     <div class="inside"><?php print $content['top']; ?></div>
   </div>
-  
-  <div class="panel-panel grid-6">
+
+  <div class="panel-panel grid-6 eisberger-topleft">
     <div class="inside"><?php print $content['topleft']; ?></div>
   </div>
-  <div class="panel-panel grid-6">
+  <div class="panel-panel grid-6 eisberger-topright">
     <div class="inside"><?php print $content['topright']; ?></div>
   </div>
-  
-  <div class="panel-panel grid-3 pane-hide-mobile">
+
+  <div class="panel-panel grid-3 eisberger-smallleft">
     <div class="inside"><?php print $content['smallleft']; ?></div>
   </div>
-  <div class="panel-panel grid-9 pane-scale-mobile">
+  <div class="panel-panel grid-9 eisberger-bigright">
     <div class="inside"><?php print $content['bigright']; ?></div>
   </div>
-  
-  <div class="panel-panel grid-3">
+
+  <div class="panel-panel grid-3 eisberger-middleleft">
     <div class="inside"><?php print $content['middleleft']; ?></div>
   </div>
-  <div class="panel-panel grid-3">
+  <div class="panel-panel grid-3 eisberger-middleleftcenter">
     <div class="inside"><?php print $content['middleleftcenter']; ?></div>
   </div>
-  <div class="panel-panel grid-3">
+  <div class="panel-panel grid-3 eisberger-middlerightcenter">
     <div class="inside"><?php print $content['middlerightcenter']; ?></div>
   </div>
-  <div class="panel-panel grid-3">
+  <div class="panel-panel grid-3 eisberger-middleright">
     <div class="inside"><?php print $content['middleright']; ?></div>
   </div>
-  
-  <div class="panel-panel grid-9">
+
+  <div class="panel-panel grid-9 eisberger-bigleft">
     <div class="inside"><?php print $content['bigleft']; ?></div>
   </div>
-  <div class="panel-panel grid-3">
+  <div class="panel-panel grid-3 eisberger-smallright">
     <div class="inside"><?php print $content['smallright']; ?></div>
   </div>
-  
-  <div class="panel-panel grid-6">
+
+  <div class="panel-panel grid-6 eisberger-bottomleft">
     <div class="inside"><?php print $content['bottomleft']; ?></div>
   </div>
-  <div class="panel-panel grid-6">
+  <div class="panel-panel grid-6 eisberger-bottomright">
     <div class="inside"><?php print $content['bottomright']; ?></div>
   </div>
-  
-  <div class="panel-panel grid-3">
+
+  <div class="panel-panel grid-3 eisberger-bottommiddleleft">
     <div class="inside"><?php print $content['bottommiddleleft']; ?></div>
   </div>
-  <div class="panel-panel grid-3">
+  <div class="panel-panel grid-3 eisberger-bottommiddleleftcenter">
     <div class="inside"><?php print $content['bottommiddleleftcenter']; ?></div>
   </div>
-  <div class="panel-panel grid-3">
+  <div class="panel-panel grid-3 eisberger-bottommiddlerightcenter">
     <div class="inside"><?php print $content['bottommiddlerightcenter']; ?></div>
   </div>
-  <div class="panel-panel grid-3">
+  <div class="panel-panel grid-3 eisberger-bottommiddleright">
     <div class="inside"><?php print $content['bottommiddleright']; ?></div>
   </div>
-  
-  <div class="panel-panel grid-12">
+
+  <div class="panel-panel grid-12 eisberger-bottom">
     <div class="inside"><?php print $content['bottom']; ?></div>
   </div>
 </div>
